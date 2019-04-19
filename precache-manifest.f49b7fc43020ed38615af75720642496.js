7ef53b072988b5bb1f5baa660694436645f00400
gonzaga <devochka.v.kube@gmail.com>
2019-04-19 18:09:05 +0200

    Updates

---
+++ precache-manifest.f49b7fc43020ed38615af75720642496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7901a29fcaae49ad50d",
+    "url": "/static/css/main.9d87f59c.chunk.css"
+  },
+  {
+    "revision": "e7901a29fcaae49ad50d",
+    "url": "/static/js/main.4e073b9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a1206dd36f7b977df67b8eaa7f739817",
+    "url": "/index.html"
+  }
+];
